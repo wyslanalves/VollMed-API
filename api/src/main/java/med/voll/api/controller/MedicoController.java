0773f7119dc1858c059f501f4434de6b3ee051a8
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-01 19:02:40 -0300

    Exclusão Lógica

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,21 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedicos;
 import med.voll.api.medico.DadosCadastroMedicos;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -39,8 +42,29 @@ public class MedicoController {
 	*/
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listar(Pageable paginacao){
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
 		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
+	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedicos dados) {
+		var medico = medicoRepository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+	}
+	
+	/*
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deletar(@PathVariable Long id) {
+		medicoRepository.deleteById(id);
+	}
+	*/
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void deletar(@PathVariable Long id) {
+		var medico = medicoRepository.getReferenceById(id);
+		medico.excluir();	}
 	
 }
--- api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -2,6 +2,7 @@ package med.voll.api.medico;
 
 public record DadosListagemMedico(
 		
+		Long id,
 		String nome,
 		String email,
 		String crm,
@@ -9,6 +10,10 @@ public record DadosListagemMedico(
 		
 		){
 	public DadosListagemMedico(Medico medico) {
-		this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+		this(medico.getId(), 
+				medico.getNome(), 
+				medico.getEmail(), 
+				medico.getCrm(), 
+				medico.getEspecialidade());
 	}
 }
