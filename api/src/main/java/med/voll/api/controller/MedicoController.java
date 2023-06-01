11f092be83f3a6e5baf93c0176f0441df8dafca2
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-01 17:16:08 -0300

    Criando Repository

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,24 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
+	@Autowired
+	private MedicoRepository medicoRepository;
+	
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroMedicos dados) {
-		System.out.println(dados);
+		medicoRepository.save(new Medico(dados));
 	}
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -24,6 +24,14 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
+	public Medico(DadosCadastroMedicos dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.crm = dados.crm();
+		this.especialidade  = dados.especialidade();
+		this.endereco = new Endereco(dados.endereco());
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
