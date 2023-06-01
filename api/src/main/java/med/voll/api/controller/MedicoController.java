a5911a6f08dacc429de181019e95f1240c938571
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-01 17:33:14 -0300

    Validação com Bean Validation

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -20,7 +21,7 @@ public class MedicoController {
 	
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody DadosCadastroMedicos dados) {
+	public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados) {
 		medicoRepository.save(new Medico(dados));
 	}
 }
