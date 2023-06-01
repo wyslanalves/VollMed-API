1fbcdeaf897e4898526e0e9141342fc42d875d9c
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-01 17:24:33 -0300

    Implementando Flyway (migration)

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.medico.DadosCadastroMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -18,6 +19,7 @@ public class MedicoController {
 	private MedicoRepository medicoRepository;
 	
 	@PostMapping
+	@Transactional
 	public void cadastrar(@RequestBody DadosCadastroMedicos dados) {
 		medicoRepository.save(new Medico(dados));
 	}
