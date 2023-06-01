4edfbcbe60fe35aa6ab5e94c5e66b1cca6a7b41c
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-01 18:11:12 -0300

    Método de Listagem

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -24,4 +28,11 @@ public class MedicoController {
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados) {
 		medicoRepository.save(new Medico(dados));
 	}
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+	}
+	
+	
 }
